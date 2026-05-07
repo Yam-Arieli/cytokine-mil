00fdac314e4def4756385075ff173168b4e6aa7d
Yam Arieli <arieliyam111@gmail.com>
2026-05-07 10:48:57 +0300

    Fix print_names venv path

--- scripts/print_names.py
+++ scripts/print_names.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0, "/cs/labs/mornitzan/yam.arieli/venvs/biovenv/lib/python3.11/site-packages")
 import pickle
 p = "/cs/labs/mornitzan/yam.arieli/cytokine-mil/results/oesinghaus_full/new_seeds_seed1/experiment_geo_pbs_rel/latent_geometry.pkl"
 d = pickle.load(open(p, "rb"))
